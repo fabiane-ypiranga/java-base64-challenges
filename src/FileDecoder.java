8568b9c74d6ba719b1ff4e1116d8853c40af2a41
Fabiane <fabiane.ypiranga@dedalus.com>
2026-04-28 09:53:11 -0300

    feat: implement Base64 challenges (encode/decode string and file)

---
+++ src/FileDecoder.java
@@ -0,0 +1,19 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+
+public class FileDecoder {
+
+    public static void main(String[] args) throws Exception {
+
+        Path inputPath = Path.of("output.txt");
+        String base64 = Files.readString(inputPath);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(base64);
+
+        Path outputPath = Path.of("restored.txt");
+        Files.write(outputPath, decodedBytes);
+
+        System.out.println("Arquivo restaurado com sucesso!");
+    }
+}
